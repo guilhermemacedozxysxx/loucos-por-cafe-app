bf233b259789d54bce9d7e775fe536a2bb9343dc
guilhermemacedozxysxx <contato.guilhermemacedo.dev@gmail.com>
2026-09-06 11:39:57 -0300

    feat: Adicionar dados iniciais da cafeteria.

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -8,6 +8,18 @@ const prismaClient = new PrismaClient();
 const main = async () => {
   await prismaClient.$transaction(async (tx: any) => {
     await tx.coffeeShop.deleteMany();
+    
+    const coffeeShop = await tx.coffeeShop.create({
+      data: {
+        name: "Loucos Por Café",
+        slug: "loucos-por-cafe",
+        description: "Eleita a melhor cafeteria® de 2026",
+        avatarImageUrl:
+          "https://z58avo7iib.ufs.sh/f/gn7SbfGB9utS5KIzJREJEkXyUi8vqW4Ib1SNQgYdpzMmRxLj",
+        coverImageUrl:
+          "https://z58avo7iib.ufs.sh/f/gn7SbfGB9utSXpaquL7KCT6rObB5qvh3As1MSwQ28toxupl0",
+      },
+    });
   });
 };
 
