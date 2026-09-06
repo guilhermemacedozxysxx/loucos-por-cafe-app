abd84f232e74f72154a4096d59a6185918757807
guilhermemacedozxysxx <contato.guilhermemacedo.dev@gmail.com>
2026-09-06 11:42:41 -0300

    feat: Adicionar produtos da categoria de lançamentos.

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -27,6 +27,85 @@ const main = async () => {
         coffeeShopId: coffeeShop.id,
       },
     });
+
+        await tx.product.createMany({
+      data: [
+        {
+          name: "Montanha de Cookies Tradicionais®",
+          description:
+            "Cookies artesanais empilhados, massa amanteigada, textura macia por dentro e crocante por fora, recheio generoso de chocolate.",
+          price: 39.9,
+          imageUrl:
+            "https://z58avo7iib.ufs.sh/f/gn7SbfGB9utStRJW9eC13ENFPmvHUc2VwjAabLuzfoeWhdYg",
+          menuCategoryId: lancamentosCategory.id,
+          coffeeShopId: coffeeShop.id,
+          ingredients: [
+            "Farinha de trigo selecionada",
+            "Cookies artesanais",
+            "Massa amanteigada",
+            "Nutella®",
+            "Recheio cremoso",
+            "Cobertura extra de creme de avelã",
+            "Gotas de chocolate",
+          ],
+        },
+        {
+          name: "Montanha Suprema de Cookies®",
+          description:
+            "Cookies artesanais empilhados, massa amanteigada, recheio cremoso de Nutella®, creme de avelã intenso e cobertura extra.",
+          price: 49.9,
+          imageUrl:
+            "https://z58avo7iib.ufs.sh/f/gn7SbfGB9utStRJW9eC13ENFPmvHUc2VwjAabLuzfoeWhdYg",
+          menuCategoryId: lancamentosCategory.id,
+          coffeeShopId: coffeeShop.id,
+          ingredients: [
+            "Farinha de trigo selecionada",
+            "Cookies artesanais",
+            "Massa amanteigada",
+            "Nutella® X2",
+            "Recheio cremoso",
+            "Cobertura extra de creme de avelã",
+            "Gotas de chocolate",
+          ],
+        },
+        {
+          name: "Cookie Artesanal Individual®",
+          description:
+            "Cookie recém-assado, massa amanteigada, interior macio, gotas de chocolate derretidas e sabor marcante.",
+          price: 9.9,
+          imageUrl:
+            "https://z58avo7iib.ufs.sh/f/gn7SbfGB9utSXZ1JCt7KCT6rObB5qvh3As1MSwQ28toxupl0",
+          menuCategoryId: lancamentosCategory.id,
+          coffeeShopId: coffeeShop.id,
+          ingredients: [
+            "Farinha de trigo selecionada",
+            "Massa amanteigada",
+            "Cookies artesanais",
+            "Gotas de chocolate",
+            "Essência de baunilha natural",
+            "Fermento suave para leve crocância",
+          ],
+        },
+        {
+          name: "Cappuccino Cremoso Especial®",
+          description:
+            "Café espresso encorpado, leite vaporizado, espuma cremosa e finalização suave de chocolate ou canela.",
+          price: 14.9,
+          imageUrl:
+            "https://z58avo7iib.ufs.sh/f/gn7SbfGB9utSxPwhVHBB5IkSCEM4u0Zd63X1JiN2RW8Qhpfc",
+          menuCategoryId: lancamentosCategory.id,
+          coffeeShopId: coffeeShop.id,
+          ingredients: [
+            "Café espresso selecionado",
+            "Leite vaporizado cremoso",
+            "Espuma de leite aveludada",
+            "Chocolate em pó premium",
+          ],
+        },
+      ],
+    });
+
+    
   });
 };
 
