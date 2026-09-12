6580eb177bb9ae57ec628e87a54239eda5a561f2
guilhermemacedozxysxx <contato.guilhermemacedo.dev@gmail.com>
2026-09-11 22:42:36 -0300

    feat: Criar componente de splash screen.

---
+++ src/components/ui/splash-screen.tsx
@@ -0,0 +1,9 @@
+const SplashScreen = () => {
+  return (
+    <div className="flex min-h-screen items-center justify-center">
+      <h1>Loucos por Café</h1>
+    </div>
+  );
+};
+
+export default SplashScreen;
