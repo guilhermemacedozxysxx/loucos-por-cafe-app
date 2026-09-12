219ba90b8256888190facf8b74a9edc4aa5b1d7a
guilhermemacedozxysxx <contato.guilhermemacedo.dev@gmail.com>
2026-09-11 23:02:48 -0300

    feat: Adicionar background à splash screen.

--- src/components/ui/splash-screen.tsx
+++ src/components/ui/splash-screen.tsx
@@ -1,7 +1,11 @@
 const SplashScreen = () => {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-[--background]">
-      <h1>Loucos por Café</h1>
+    <div className="relative flex min-h-screen items-center justify-center overflow-hidden bg-[--background]">
+      <div className="absolute inset-0 bg-[url('/images/BANNER-LOUCOS-POR-CAFE.png')] bg-[length:900px_auto] bg-repeat opacity-30" />
+
+      <div className="relative z-10">
+        <h1>Loucos por Café</h1>
+      </div>
     </div>
   );
 };
