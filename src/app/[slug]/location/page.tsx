11d86d40a1a420a93b0c35ccea5ffb1e04d3b3be
guilhermemacedozxysxx <contato.guilhermemacedo.dev@gmail.com>
2026-09-13 15:28:52 -0300

    feat: Adicionar texto de boas-vindas à página de localização.

---
+++ src/app/[slug]/location/page.tsx
@@ -0,0 +1,9 @@
+const LocationPage = () => {
+  return (
+    <header className="h-6 w-full mt-16 flex items-center text-center justify-center">
+      <p className="font-lufga font-semibold text-lg uppercase">Bem Vindo</p>
+    </header>
+  );
+};
+
+export default LocationPage;
