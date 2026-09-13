100c8ea059639d84c2eb21928095cf02351696c0
guilhermemacedozxysxx <contato.guilhermemacedo.dev@gmail.com>
2026-09-13 16:40:16 -0300

    style: Ajustar fontes e estilos da tela de permissão de localização.

--- src/app/[slug]/location/page.tsx
+++ src/app/[slug]/location/page.tsx
@@ -4,24 +4,34 @@ const LocationPage = () => {
   return (
     <>
       <header className="h-6 w-full mt-16 mb-40 flex items-center text-center justify-center">
-        <p className="font-lufga font-semibold text-lg uppercase">Bem Vindo</p>
+        <p className="font-lufga font-semibold text-xl uppercase">Bem Vindo</p>
       </header>
 
       <main>
-        <div className="flex">
-          <div className="relative flex h-14 w-14 items-center justify-center rounded-lg bg-[--primary]">
+        <div className="flex gap-3 ml-14">
+          <div className="relative flex h-20 w-20 items-center justify-center rounded-2xl bg-[--primary]">
             <Image
               src="/images/LOGO.svg"
               alt="Loucos por Café"
               width={330}
               height={330}
               priority
-              className="mt-2 mr-2 h-20 w-20 max-w-none"
+              className="mt-2 mr-2 h-28 w-28 max-w-none"
             />
           </div>
 
-          <div>
-            
+          <div className="flex flex-col">
+            <h1 className="font-lufga font-semibold text-xl text-black">
+              Loucos por Café®
+            </h1>
+
+            <p className="font-lufga font-medium text-md text-[--secondary] opacity-60">
+              Cafeteria • 15 - 20 min
+            </p>
+
+            <span className="font-lufga font-medium text-sm text-[--primary]">
+              Entrega gratuita
+            </span>
           </div>
         </div>
       </main>
