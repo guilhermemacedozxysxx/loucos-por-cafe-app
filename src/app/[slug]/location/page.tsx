c883575171b7787e1db3a1cf9196f8d626333fb1
guilhermemacedozxysxx <contato.guilhermemacedo.dev@gmail.com>
2026-09-16 20:39:12 -0300

    feat: Tratar erros de acesso à localização.

--- src/app/[slug]/location/page.tsx
+++ src/app/[slug]/location/page.tsx
@@ -1,27 +1,44 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
+
 import Image from "next/image";
 
 const LocationPage = () => {
   const handleLocationPermission = () => {
-  if (!navigator.geolocation) {
-    console.log("Geolocalização não suportada pelo navegador.");
-    return;
-  }
+    if (!navigator.geolocation) {
+      console.log("Geolocalização não suportada pelo navegador.");
+      return;
+    }
 
-  navigator.geolocation.getCurrentPosition(
-    (position) => {
-      const { latitude, longitude} = position.coords;
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        const { latitude, longitude } = position.coords;
 
-      console.log("Latitude", latitude);
-      console.log("Longitude", longitude);
-    },
-    () => {
-      console.log("Não foi possível acessar a localização.");
-    },
-  );
-};
+        console.log("Latitude:", latitude);
+        console.log("Longitude:", longitude);
+      },
+      (error) => {
+        switch (error.code) {
+          case error.PERMISSION_DENIED:
+            console.log("Permissão de localização negada.");
+            break;
+
+          case error.POSITION_UNAVAILABLE:
+            console.log("Localização indisponível.");
+            break;
+
+          case error.TIMEOUT:
+            console.log("Tempo limite para obter a localização.");
+            break;
+
+          default:
+            console.log("Erro desconhecido ao obter a localização.");
+        }
+      },
+    );
+  };
+
   return (
     <div className="flex min-h-dvh flex-col">
       <header className="flex w-full items-center justify-center pt-14 text-center">
@@ -31,7 +48,7 @@ const LocationPage = () => {
       </header>
 
       <main className="flex flex-1 flex-col px-5 pt-20">
-        <div className="flex items-center gap-3 ml-9">
+        <div className="ml-9 flex items-center gap-3">
           <div className="relative flex h-20 w-20 shrink-0 items-center justify-center rounded-2xl bg-[--primary]">
             <Image
               src="/images/LOGO.svg"
