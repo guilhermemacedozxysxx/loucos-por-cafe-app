475f17b6503b7a5c78418dde1c25ae88a441a014
guilhermemacedozxysxx <contato.guilhermemacedo.dev@gmail.com>
2026-09-15 22:28:53 -0300

    feat: Vincular solicitação de localização ao botão.

--- src/app/[slug]/location/page.tsx
+++ src/app/[slug]/location/page.tsx
@@ -68,7 +68,7 @@ const LocationPage = () => {
       </main>
 
       <footer className="px-5 pb-6">
-        <Button className="w-full rounded-full border-none bg-[--primary] p-6 font-lufga text-base font-medium text-[--foreground] active:bg-[--primary] active:text-[--foreground] focus:bg-[--primary] focus:text-[--foreground]">
+        <Button onClick={handleLocationPermission} className="w-full rounded-full border-none bg-[--primary] p-6 font-lufga text-base font-medium text-[--foreground] active:bg-[--primary] active:text-[--foreground] focus:bg-[--primary] focus:text-[--foreground]">
           Permitir localização
         </Button>
       </footer>
