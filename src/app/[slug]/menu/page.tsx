212bac0f898d942a9bdb70c6b6c020d0a12dc2a1
guilhermemacedozxysxx <contato.guilhermemacedo.dev@gmail.com>
2026-09-19 14:19:01 -0300

    feat: Integrar dados da cafeteria à página de categorias do menu.

--- src/app/[slug]/menu/page.tsx
+++ src/app/[slug]/menu/page.tsx
@@ -1,9 +1,19 @@
+import { prisma } from "@/lib/prisma";
+
+import CoffeeShopCategories from "./components/categories";
 import CoffeeShopHeader from "./components/header";
 
-const MenuCategory = () => {
+const MenuCategory = async () => {
+  const coffeeShop = await prisma.coffeeShop.findFirst();
+
+  if (!coffeeShop) {
+    return null;
+  }
+
   return (
     <div>
       <CoffeeShopHeader />
+      <CoffeeShopCategories coffeeshop={coffeeShop}/>
     </div>
   );
 };
