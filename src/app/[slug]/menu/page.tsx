65826c2b5ee643313497206b87a9d2dc96e097b6
guilhermemacedozxysxx <contato.guilhermemacedo.dev@gmail.com>
2026-09-19 13:47:34 -0300

    feat: Adicionar banner da cafeteria à página de categorias do menu.

--- src/app/[slug]/menu/page.tsx
+++ src/app/[slug]/menu/page.tsx
@@ -1,5 +1,19 @@
+import Image from "next/image";
+
 const MenuCategory = () => {
-  return (  );
-}
- 
+  return (
+    <div>
+      <div className="relative h-[250px] w-full">
+        ]
+        <Image
+          src="/images/BANNER_CAFETERIA.jpg"
+          alt="BANNER CAFETERIA"
+          fill
+          className="object-cover"
+        />
+      </div>
+    </div>
+  );
+};
+
 export default MenuCategory;
