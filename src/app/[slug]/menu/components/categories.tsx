e891d092d2d8fdf791e8e4131f98582550f6f9c8
guilhermemacedozxysxx <contato.guilhermemacedo.dev@gmail.com>
2026-09-20 02:29:22 -0300

    refactor: Ajustar tipagem das categorias da cafeteria.

--- src/app/[slug]/menu/components/categories.tsx
+++ src/app/[slug]/menu/components/categories.tsx
@@ -14,9 +14,8 @@ interface CoffeeShopCategoriesProps {
   coffeeshop: CoffeeShopWithCategories;
 }
 
-const CoffeeShopCategories = ({coffeeshop}: CoffeeShopCategoriesProps) => {
-  return ( 
-    
+const CoffeeShopCategories = ({ coffeeshop }: CoffeeShopCategoriesProps) => {
+  return (
     <div className="relative z-50 mt-[-1.5rem] rounded-t-3xl pt-1 bg-white">
       <div className="flex items-center gap-3 m-5">
         <div className="flex justify-center w-14 h-14 bg-[--primary] rounded-xl">
@@ -30,32 +29,44 @@ const CoffeeShopCategories = ({coffeeshop}: CoffeeShopCategoriesProps) => {
         </div>
 
         <div>
-          <h2 className="font-lufga font-semibold text-lg">{coffeeshop.name}</h2>
-          <p className="font-lufga font-normal text-[secondary] opacity-40 text-sm">Cafeteria</p>
+          <h2 className="font-lufga font-semibold text-lg">
+            {coffeeshop.name}
+          </h2>
+          <p className="font-lufga font-normal text-[secondary] opacity-40 text-sm">
+            Cafeteria
+          </p>
         </div>
 
         <div className="w-16 h-6 ml-14 mt-2 border border-neutral-200 bg-white rounded-full flex px-3 gap-1">
-          <div className="mt-[2px]"><i className="fi fi-sr-star text-[--primary]"></i></div>
+          <div className="mt-[2px]">
+            <i className="fi fi-sr-star text-[--primary]"></i>
+          </div>
           <div className="font-lufga font-medium">5.0</div>
         </div>
       </div>
 
-        <div className="m-5 -mt-2 font-lufga text-[--primary] flex gap-1 items-center text-sm">
-          <ClockIcon size={16}/>
-          <p>Aberto até as 23:00</p>
-        </div>
+      <div className="m-5 -mt-2 font-lufga text-[--primary] flex gap-1 items-center text-sm">
+        <ClockIcon size={16} />
+        <p>Aberto até as 23:00</p>
+      </div>
 
-        <ScrollArea className="font-lufga font-semibold text-[--muted-foreground] w-full">
-          <div className="full w-max gap-4 p-5">
-            {coffeeshop.menuCategories.map(category   => (
-              <Button key={category.id} variant="secondary" size="sm">
-                {category.name}
-              </Button>))}
-          </div>
-          <ScrollBar orientation="horizontal"/>
-        </ScrollArea>
+      <ScrollArea className="font-lufga font-semibold text-[--muted-foreground] w-full">
+        <div className="flex w-max gap-4 p-5 -mt-4">
+          {coffeeshop.menuCategories.map((category) => (
+            <Button
+              key={category.id}
+              variant="secondary"
+              size="sm"
+              className="rounded-full"
+            >
+              {category.name}
+            </Button>
+          ))}
+        </div>
+        <ScrollBar orientation="horizontal" />
+      </ScrollArea>
     </div>
-   );
-}
+  );
+};
 
 export default CoffeeShopCategories;
