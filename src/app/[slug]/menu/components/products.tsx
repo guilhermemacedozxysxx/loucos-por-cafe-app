b31b56ae2e462b24b31a354a9f0bab9ef7d31b48
guilhermemacedozxysxx <contato.guilhermemacedo.dev@gmail.com>
2026-09-21 21:10:16 -0300

    feat: Integrar produtos ao componente Products.

--- src/app/[slug]/menu/components/products.tsx
+++ src/app/[slug]/menu/components/products.tsx
@@ -1,5 +1,21 @@
-const Products = () => {
-  return (  );
+import { Product } from "@prisma/client";
+
+interface ProductsProps {
+  products: Product[];
 }
- 
+
+const Products = ({ products }: ProductsProps) => {
+  return (
+    <div className="space-y-3">
+      {products.map((product) => (
+        <div key={product.id}>
+          <h3 className="font-lufga text-sm font-medium">
+            {product.name}
+          </h3>
+        </div>
+      ))}
+    </div>
+  );
+};
+
 export default Products;
