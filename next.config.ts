495c6f5142e9ed9335888f3da2f6dd1de93d99da
guilhermemacedozxysxx <contato.guilhermemacedo.dev@gmail.com>
2026-09-22 23:04:10 -0300

    feat: Exibir imagens dos produtos no menu.

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "z58avo7iib.ufs.sh",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
--- src/app/[slug]/menu/components/products.tsx
+++ src/app/[slug]/menu/components/products.tsx
@@ -1,4 +1,5 @@
 import { Product } from "@prisma/client";
+import Image from "next/image";
 
 interface ProductsProps {
   products: Product[];
@@ -8,24 +9,39 @@ const Products = ({ products }: ProductsProps) => {
   return (
     <div className="space-y-3 px-6">
       {products.map((product) => (
-        <div key={product.id}>
-          <h3 className="font-lufga text-sm">{product.name}</h3>
+        <div
+          key={product.id}
+          className="flex items-center justify-between gap-4"
+        >
+          {/* TEXTO */}
+          <div className="flex-1">
+            <h3 className="font-lufga text-sm">
+              {product.name}
+            </h3>
 
-          <p className="font-lufga text-sm text-[--muted-foreground] line-clamp-2">
-            {product.description}
-          </p>
-          <p className="font-lufga font-semibold text-lg text-[--secondary] mt-3">
-            {new Intl.NumberFormat("pt-BR", {
-              style: "currency",
-              currency: "BRL",
-            }).format(product.price)}
-          </p>
+            <p className="line-clamp-2 font-lufga text-sm text-[--muted-foreground]">
+              {product.description}
+            </p>
+
+            <p className="mt-3 font-lufga text-lg font-semibold text-[--secondary]">
+              {new Intl.NumberFormat("pt-BR", {
+                style: "currency",
+                currency: "BRL",
+              }).format(product.price)}
+            </p>
+          </div>
+
+          {/* IMAGEM */}
+          <div className="relative h-[82px] w-[82px] shrink-0">
+            <Image
+              src={product.imageUrl}
+              alt={product.name}
+              fill
+              className="rounded-lg object-cover"
+            />
+          </div>
         </div>
       ))}
-
-      <div className="relative min-h-[82px] min-w-[120px]">
-
-      </div>
     </div>
   );
 };
